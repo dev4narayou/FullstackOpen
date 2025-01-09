9ed3000fec679e012bb9c5a804c568be213faa64
dev <devmanojkumar08@gmail.com>
2025-01-10 10:54:27 +1100

    exercise 2.11 (phonebook)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,28 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import Header from "./components/Header";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import People from "./components/People";
+import axios from "axios";
 
 const App = () => {
-  // const [persons, setPersons] = useState([]);
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
+  // const [persons, setPersons] = useState([
+  //   { name: "Arto Hellas", number: "040-123456", id: 1 },
+  //   { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+  //   { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+  //   { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  // ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  })
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
