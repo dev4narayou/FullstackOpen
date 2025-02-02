cf10e7fba0cdb92e9b1679a80b6e37d7457cce63
dev <devmanojkumar08@gmail.com>
2025-02-02 17:29:54 +1100

    Until 4.2 Blog List, step 2 "Refactor the application into separate modules as shown earlier in this part of the course material."

---
+++ part4/blog-list/app.js
@@ -0,0 +1,19 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const mongoose = require("mongoose");
+
+const mongoUrl = `${process.env.MONGODB_URI}`;
+mongoose.connect(mongoUrl).catch((error) => {
+  console.error("error connecting to MongoDB:", error.message);
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/blogs', blogsRouter);
+
+
+module.exports = app;
