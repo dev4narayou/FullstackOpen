9df72766a5e19a285782b8c129c53475f24efe2f
dev <devmanojkumar08@gmail.com>
2025-02-03 20:49:17 +1100

    updated notes app to 4.8 end (many bugs now, to be refactored in the future)

--- part3/notes-backend/controllers/notes.js
+++ part3/notes-backend/controllers/notes.js
@@ -1,8 +1,10 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(notes)
 })
 
@@ -18,30 +20,36 @@ notesRouter.put('/:id', async (request, response) => {
   response.json(updatedNote)
 })
 
-notesRouter.post("/", async (request, response) => {
-  const body = request.body;
+notesRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  const user = await User.findById(body.userId)
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-  });
+    user: user._id
+  })
 
-  const savedNote = await note.save();
-  response.status(201).json(savedNote);
-});
+  const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
 
-notesRouter.get("/:id", async (request, response) => {
-  const note = await Note.findById(request.params.id);
+  response.status(201).json(savedNote)
+})
+
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
   if (note) {
-    response.json(note);
+    response.json(note)
   } else {
-    response.status(404).end();
+    response.status(404).end()
   }
-});
+})
 
-notesRouter.delete("/:id", async (request, response) => {
-  await Note.findByIdAndDelete(request.params.id);
-  response.status(204).end();
-});
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
 
 module.exports = notesRouter
