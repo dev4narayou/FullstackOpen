54ae5ab2ea44bef138f885f0cdb8199b6d531cde
dev <devmanojkumar08@gmail.com>
2025-02-09 12:25:53 +1100

    4.17: Blog List Expansion, step 5

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,18 +1,26 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({})
+    .populate('user', { username: 1, name: 1 });
+  response.json(blogs);
+
 });
 
-blogsRouter.post("/", (request, response) => {
-  const blog = new Blog(request.body);
+blogsRouter.post("/", async (request, response) => {
+  const { title, author, url, likes } = request.body;
+  const user = await User.findOne();
+  const id = user._id;
+  const blog = Blog({title, author, url, likes, user: id})
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
+  const savedBlog = await blog.save()
+  const populatedBlog = await Blog.findById(savedBlog._id).populate("user", {
+    username: 1,
+    name: 1,
   });
+  return response.status(201).json(populatedBlog);
 });
 
 blogsRouter.delete("/:id", (request, response) => {
