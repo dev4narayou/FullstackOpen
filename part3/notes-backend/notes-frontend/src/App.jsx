f19b0b43836f9f2269354eee842ee1deec03bde0
dev <devmanojkumar08@gmail.com>
2025-02-12 13:49:52 +1100

    refactored notes frontend to be nested within backend, with build script

--- part3/notes-backend/notes-frontend/src/App.jsx
+++ part3/notes-backend/notes-frontend/src/App.jsx
@@ -9,6 +9,8 @@ const App = () => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
     noteService
@@ -18,13 +20,18 @@ const App = () => {
       })
   }, [])
 
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log("logging in with", username, password);
+  };
+
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
       important: Math.random() > 0.5,
     }
-  
+
     noteService
       .create(noteObject)
         .then(returnedNote => {
@@ -36,7 +43,7 @@ const App = () => {
   const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
-  
+
     noteService
       .update(id, changedNote)
         .then(returnedNote => {
@@ -64,30 +71,50 @@ const App = () => {
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+          show {showAll ? "important" : "all"}
         </button>
-      </div>      
+      </div>
       <ul>
-        {notesToShow.map(note => 
+        {notesToShow.map((note) => (
           <Note
             key={note.id}
             note={note}
             toggleImportance={() => toggleImportanceOf(note.id)}
           />
-        )}
+        ))}
       </ul>
       <form onSubmit={addNote}>
-      <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
+        <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
       <Footer />
     </div>
-  )
+  );
 }
 
 export default App
