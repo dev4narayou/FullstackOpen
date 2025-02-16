e21c893a15c24fbfd9a2a868b481f1279102a3e0
dev <devmanojkumar08@gmail.com>
2025-02-17 10:48:42 +1100

    5.7 Blog List Frontend, step 7

--- part5/blog-list/bloglist-frontend/src/components/Blog.jsx
+++ part5/blog-list/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {useState, forwardRef} from 'react';
 
-export default Blog
+const Blog = forwardRef(({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const hideWhenVisible = { display: visible ? "none" : "" };
+  const showWhenVisible = { display: visible ? "" : "none" };
+
+  return (
+    <div style={{ border: "1px solid black", padding: "5px", margin: "5px" }}>
+      <div style={showWhenVisible}>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={hideWhenVisible}>
+        {blog.title}
+        <button onClick={toggleVisibility}>hide</button>
+        <br></br>
+        {blog.url}
+        <br></br>
+        {blog.likes}
+        <br></br>
+        {blog.author}
+      </div>
+    </div>
+  );
+});
+
+export default Blog;
