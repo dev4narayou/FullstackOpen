26c965a843ca5a85e277d450759d3d40f792b84e
dev <devmanojkumar08@gmail.com>
2025-02-17 11:53:59 +1100

    5.10: Blog List Frontend, step 10

--- part5/blog-list/bloglist-frontend/src/App.jsx
+++ part5/blog-list/bloglist-frontend/src/App.jsx
@@ -46,9 +46,11 @@ const App = () => {
 
   const Blogs = () => (
     <>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} updateBlog={handleBlogUpdate} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} updateBlog={handleBlogUpdate} />
+        ))}
     </>
   );
 
