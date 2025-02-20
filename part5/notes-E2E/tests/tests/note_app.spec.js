15704e9d73cc8bf5b11cbd28064a570ca83d978e
dev <devmanojkumar08@gmail.com>
2025-02-20 18:52:43 +1100

    E2E testing implementation started for notes app. Implemented up to (inclusive) testing note creation.

---
+++ part5/notes-E2E/tests/tests/note_app.spec.js
@@ -0,0 +1,41 @@
+const { test, describe, expect, beforeEach } = require('@playwright/test')
+
+
+describe('Note app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('front page can be opened', async ({ page }) => {
+    const locator = await page.getByText('Notes')
+    await expect(locator).toBeVisible()
+    await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2024')).toBeVisible()
+  })
+
+  test('login form can be opened', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('button', { name: 'log in' }).click()
+    await page.getByTestId('username').fill('dev4narayou')
+    await page.getByTestId('password').fill('devspassword')
+    await page.getByRole('button', { name: 'log in' }).click()
+    await expect(page.getByText('Dev K logged in')).toBeVisible()
+  })
+
+  describe('when logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByRole('button', { name: 'log in' }).click()
+      await page.getByTestId('username').fill('dev4narayou')
+      await page.getByTestId('password').fill('devspassword')
+      await page.getByRole('button', { name: 'log in' }).click()
+    })
+
+    test('a new note can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'new note' }).click()
+      await page.getByTestId('note-input').fill('a note created by playwright')
+      await page.getByRole('button', { name: 'save' }).click()
+      await expect(page.getByText('a note created by playwright')).toBeVisible()
+    })
+  })
+})
+
