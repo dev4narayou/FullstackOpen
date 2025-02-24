d29fe654087ab1408204707abec792c8815f57ce
dev <devmanojkumar08@gmail.com>
2025-02-24 12:29:15 +1100

    5.18: Blog List End To End Testing, step 2

--- part5/blog-list-E2E/tests/blog_list.spec.js
+++ part5/blog-list-E2E/tests/blog_list.spec.js
@@ -1,16 +1,39 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
 
 describe("Blog app", () => {
-  beforeEach(async ({ page }) => {
-    await page.goto("http://localhost:5173");
+  beforeEach(async ({ page, request }) => {
+    await request.post("/api/testing/reset");
+    const user = {
+      username: "fakeme",
+      name: "fakename",
+      password: "fakepassword",
+    };
+    await request.post("/api/users", {
+      data: user,
+    });
+    await page.goto("/");
   });
 
   test("Login form is shown", async ({ page }) => {
     // await page.getByRole("button", { name: "login" }).click();
-    await page.getByTestId("username").fill("fakeme");
-    await page.getByTestId("password").fill("fakepassword");
-    await page.getByRole("button", { name: "submit" }).click();
-    const locator = await page.getByText("fakenameSCARY logged in")
-    await expect(locator).toBeVisible();
+    await expect(page.getByTestId("username")).toBeVisible();
+    await expect(page.getByTestId("password")).toBeVisible();
+  });
+
+  describe("Login", () => {
+    test("succeeds with correct credentials", async ({ page }) => {
+      await page.getByTestId("username").fill("fakeme");
+      await page.getByTestId("password").fill("fakepassword");
+      await page.getByRole("button", { name: "submit" }).click();
+      await expect(page.getByText("fakename logged in")).toBeVisible();
+    });
+
+    test("fails with wrong credentials", async ({ page }) => {
+      await page.getByTestId("username").fill("wrongusername");
+      await page.getByTestId("password").fill("wrongpassword");
+      await page.getByRole("button", { name: "submit" }).click();
+      const locator = await page.getByText("invalid username or password");
+      await expect(locator).toBeVisible();
+    });
   });
 });
